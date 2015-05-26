9f0d526ac7c28358bc51fc89e63256f5949029c5
Fletcher Foti <fscottfoti@gmail.com>
2015-05-26 00:09:13 -0600

    do a direct count of the number of places for a specific user
    this is instead of denormalization which I do for place counts on collections and post counts on places.  Not sure which one is better, but this will get the right answer every time, even if a little bit less performant.

--- client/views/profile.js
+++ client/views/profile.js
@@ -1,3 +1,13 @@
+Template.profile.rendered = function () {
+
+    Session.set('placesByUser', 0);
+    var uid = Session.get('active_user');
+    Meteor.call('countPlacesByUser', uid, function (err, count) {
+        Session.set('placesByUser', count);
+    });
+};
+
+
 Template.profile.helpers({
     visible_places_count: function () {
         return Session.get('map_visible_places') || 0;
@@ -7,5 +17,8 @@ Template.profile.helpers({
     },
     exceeds_place_limit: function () {
         return (Session.get('map_visible_places') || 0) >= PLACE_LIMIT;
+    },
+    userPlaceCount: function () {
+        return Session.get('placesByUser');
     }
 });
