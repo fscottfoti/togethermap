84ce4ec390852a1b6199eba65d7ee2a114a64ca9
Fletcher Foti <fscottfoti@gmail.com>
2015-05-29 14:41:17 -0600

    fixing up the email layout

--- server/templates/recent.js
+++ server/templates/recent.js
@@ -22,3 +22,30 @@ recentPlacesData = function (cid) {
         recent_comments: MComments.find({collectionId: cid}, {$sort: {createDate: -1}, limit: RECENT_LIMIT}).fetch()
     }
 };
+
+
+// this was taken from telescope app, as was the emailWrapper template
+buildTemplate = function (htmlContent) {
+
+    var emailProperties = {
+        secondaryColor: '#2c3e50',
+        accentColor: '#f39c12',
+        siteName: 'Yesterday\'s News from TogetherMap',
+        tagline: 'Make maps of things, then talk about them.',
+        siteUrl: 'http://togethermap.com',
+        body: htmlContent,
+        unsubscribe: '',
+        footer: '',
+        logoUrl: '',
+        logoHeight: undefined,
+        logoWidth: undefined
+    };
+
+    var emailHTML = Handlebars.templates['layout'](emailProperties);
+
+    var inlinedHTML = juice(emailHTML);
+
+    var doctype = '<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">'
+
+    return doctype+inlinedHTML;
+};
