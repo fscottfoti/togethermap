261fef6fcfd7c05dbdaf4a033d180993f4627a44
Fletcher Foti <fscottfoti@gmail.com>
2015-06-17 22:54:29 -0700

    a few more tweaks

--- client/navbar.js
+++ client/navbar.js
@@ -43,6 +43,10 @@ Template.navItems.helpers({
         var cid = Session.get('active_collection');
         if(!cid)
             return;
+        if(Router.current().route.path())
+            // in the search route, this is set to the factual collection
+            // but we don't want to display the Factual name here
+            return;
         var c = MCollections.findOne(cid);
         if(!c)
             return;
